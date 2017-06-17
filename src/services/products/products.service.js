59c579a69bc515903c38c6abe183723c0416f6fe
Pulak Chakraborty <pulak.chakraborty@motius.local>
2017-06-17 11:20:57 +0200

    Securing the web application - Users will not be able to access product listing page of other users

--- src/services/products/products.service.js
+++ src/services/products/products.service.js
@@ -3,13 +3,15 @@
 export default class ProductsService {
 
     static get $inject(){
-        return ['$http', 'API_URL'];
+        return ['$http', 'API_URL', 'UserService', '$state'];
     }
 
-    constructor($http,API_URL) {
+    constructor($http,API_URL,UserService,$state) {
         this.$http = $http;
         this.resourceUrl = `${ API_URL }/products/`;
         this.extraUrl = 'seller/';
+        this.UserService = UserService;
+        this.$state = $state;
 
     }
 
@@ -66,7 +68,6 @@ export default class ProductsService {
     }
 
     update(product) {
-
         let url = `${ this.resourceUrl }${ product['_id'] }`;
         return this.$http.put(url,product).then(responce => {
 
@@ -78,15 +79,20 @@ export default class ProductsService {
     }
 
     listProducts(id) {
-        let url = `${ this.resourceUrl }${ this.extraUrl }${ id }`;
-        return this.$http.get(url).then(responce => {
+        // make sure that user is not able to see product listing of another user
+        let requestingUser = this.UserService.getCurrentUser();
+        if (requestingUser['_id'] === id) {
+            let url = `${ this.resourceUrl }${ this.extraUrl }${ id }`;
+            return this.$http.get(url).then(responce => {
 
-            return new Promise((resolve, reject) => {
-                resolve(responce.data);
+                return new Promise((resolve, reject) => {
+                    resolve(responce.data);
 
+                });
             });
-
-        });
+        } else {
+            this.$state.go('home',{});
+        }
 
     }
 
