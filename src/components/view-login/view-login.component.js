7564330c253b99da620aaf9e7f3ab0afa705af81
Pulak Chakraborty <pulak.chakraborty@motius.local>
2017-07-13 01:11:52 +0200

    Handle Authorization Error - through a promise handle 401 authorization error returned from backend api - display using a toast!

--- src/components/view-login/view-login.component.js
+++ src/components/view-login/view-login.component.js
@@ -21,27 +21,40 @@ class ViewLoginComponent {
 }
 
 class ViewLoginComponentController{
-    constructor($state,UserService){
+    constructor($state,UserService,$mdToast){
         this.$state = $state;
         this.UserService = UserService;
+        this.$mdToast = $mdToast;
     }
 
     $onInit() {
         this.login = {};
+        this.buttonClicked = false;
     }
 
     submit(){
+        this.buttonClicked = true;
         let user = this.login.username;
         let password = this.login.password;
         this.UserService.login(user,password).then(()=> {
             let seller = this.UserService.getCurrentUser();
             let seller_Id = seller['_id'];
-            this.$state.go('app.product.productsSeller',{sellerId: seller_Id});
+            this.$state.go('app.product.productsSeller', {sellerId: seller_Id});
+        }, responce => {
+            console.log(responce);
+            this.buttonClicked = false;
+            this.$mdToast.show(
+                this.$mdToast.simple()
+                    .toastClass('md-toast-error')
+                    .textContent('Your username and password do not match')
+                    .position('bottom right')
+                    .hideDelay(5000)
+            );
         });
     }
 
     static get $inject(){
-        return ['$state', UserService.name];
+        return ['$state', UserService.name, '$mdToast'];
     }
 
 }
